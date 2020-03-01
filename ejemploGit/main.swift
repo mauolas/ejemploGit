99e76c175651a4430ec9d1dbe6cc68f445011783
Mauricio <mauricio.olascoaga@ibm.com>
2020-03-01 16:25:22 -0600

    Initial Commit

---
+++ ejemploGit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ejemploGit
+//
+//  Created by Mauricio Olascoaga on 3/1/20.
+//  Copyright © 2020 Mauricio Olascoaga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
